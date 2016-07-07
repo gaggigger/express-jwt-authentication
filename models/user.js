96a43a5d395e002d332687c6d92ecad0c7bc2b26
sakoh <sakohartounian@yahoo.com>
2016-07-07 00:16:15 -0700

    user.js flattened with arrow syntax

--- models/user.js
+++ models/user.js
@@ -17,23 +17,18 @@ function encryptPassword(next) {
   bcrypt.genSalt(10, saltCallback(user, next));
 }
 
-function saltCallback(user, next) {
-  return function(err, salt) {
-    if (err) return next(err);
+const saltCallback = (user, next) => (err, salt) => {
+  if (err) return next(err);
 
-    bcrypt.hash(user.password, salt, null, hashCallback(user, next));
-  }
-}
+  bcrypt.hash(user.password, salt, null, hashCallback(user, next));
+};
 
-function hashCallback(user, next) {
-  return function(err, hash) {
-    if (err) return next(err);
+const hashCallback = (user, next) => (err, hash) => {
+  if (err) return next(err);
 
-    user.password = hash;
-    next();
-  }
-}
-
+  user.password = hash;
+  next();
+};
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
