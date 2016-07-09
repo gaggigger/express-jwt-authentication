30ece5c6354cea2f70d62eb35592651eb56f5dee
sakoh <sakohartounian@yahoo.com>
2016-07-09 14:55:58 -0700

    testing added

--- test/routes.js
+++ test/routes.js
@@ -1,17 +1,25 @@
 const chakram = require('chakram'),
-    expect = chakram.expect,
-    User = require('../models/user'),
-    tokenForUser = require('../helpers/token_for_user');
+      expect = chakram.expect,
+      User = require('../models/user'),
+      tokenForUser = require('../helpers/token_for_user');
 
-describe("Index Route", () => {
-    var user;
+describe("Routes", () => {
+  var user, payload;
 
-    beforeEach(() => {
-      user = new User({
-        email: "jameswest@example.com",
-        password: "password"
-      });
+  beforeEach(() => {
+    user = new User({
+      email: "jameswest@example.com",
+      password: "password"
     });
+
+    payload = {
+      body: {
+        email: user.email, password: user.password
+      }
+    };
+  });
+
+  describe("/", () => {
 
     it("should deny access to unauthenticated users", () => {
         const response = chakram.get("http://localhost:3090/");
@@ -34,4 +42,72 @@ describe("Index Route", () => {
         });
 
     });
+  });
+
+  describe("/signup",() => {
+    it("should require email", () => {
+      user.email = "";
+
+      const response = chakram.post("http://localhost:3090/signup", payload);
+
+      return expect(response).to.have.json("error", "You must provide email and password");
+    });
+
+    it("should require password", () => {
+      user.password = "";
+
+      const response = chakram.post("http://localhost:3090/signup", payload);
+
+      return expect(response).to.have.json("error", "You must provide email and password");
+    });
+
+    it("should not sign up emails that are already in user", () => {
+      user.save(err => {
+        if(err) console.log(err);
+
+        const response = chakram.post("http://localhost:3090/signup", payload);
+
+        return expect(response).to.have.json("error", "You must provide email and password");
+      });
+    });
+  });
+
+  describe("/signin", () => {
+
+    beforeEach(() => {
+      user.save();
+    });
+
+    it("should require email", () => {
+      user.email = "";
+
+      const response = chakram.post("http://localhost:3090/signin", payload);
+
+      return expect(response).to.have.status(400);
+    });
+
+    it("should require password", () => {
+      user.password = "";
+
+      const response = chakram.post("http://localhost:3090/signin", payload);
+
+      return expect(response).to.have.status(400);
+    });
+
+    it("should deny a wrong email", () => {
+      user.password = "jameswest1@example.com";
+
+      const response = chakram.post("http://localhost:3090/signin", payload);
+
+      return expect(response).to.have.status(400);
+    });
+
+    it("should deny a wrong password", () => {
+      user.password = "p@ssword";
+
+      const response = chakram.post("http://localhost:3090/signin", payload);
+
+      return expect(response).to.have.status(400);
+    });
+  });
 });
